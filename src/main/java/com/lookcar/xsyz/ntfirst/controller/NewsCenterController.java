81d3c04f5f7a2b3920a578754286b9f27558d723
kongmingliang <kongmingliang1@crc.com.hk>
2020-12-29 12:52:19 +0800

    产品介绍模块代码提交

--- src/main/java/com/lookcar/xsyz/ntfirst/controller/NewsCenterController.java
+++ src/main/java/com/lookcar/xsyz/ntfirst/controller/NewsCenterController.java
@@ -1,6 +1,11 @@
 package com.lookcar.xsyz.ntfirst.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.lookcar.xsyz.ntfirst.entity.FtNews;
+import com.lookcar.xsyz.ntfirst.entity.FtProduct;
+import com.lookcar.xsyz.ntfirst.entity.PageResultData;
 import com.lookcar.xsyz.ntfirst.service.FtNewsService;
 import com.lookcar.xsyz.ntfirst.util.ResponseUtil;
 import org.slf4j.Logger;
@@ -8,7 +13,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @author kongmingliang
@@ -24,9 +32,24 @@ public class NewsCenterController {
     private FtNewsService ftNewsService;
 
     @RequestMapping("/list")
-    public JSONObject getNewsList() {
-        logger.info("NewsCenterController.list()");
-        return ResponseUtil.getSuccessResult(ftNewsService.getNewsList());
+    public JSONObject getNewsList(@RequestParam(required = false, defaultValue = "1") int pageNum, @RequestParam(required = false, defaultValue = "5") int pageSize) {
+        logger.info("NewsCenterController.list(), page={}, pageSize={}", pageNum, pageSize);
+        if (pageNum < 1) {
+            pageNum = 1;
+        }
+        if (pageSize < 1 || pageSize > 50) {
+            pageSize = 50;
+        }
+        PageHelper.startPage(pageNum, pageSize);
+
+        List<FtNews> list = ftNewsService.getNewsList();
+        PageInfo<FtNews> pageInfo = new PageInfo<>(list);
+
+        JSONObject data = new JSONObject();
+        data.put("pageInfo", PageResultData.renderPageResultData(pageInfo));
+        data.put("news", list);
+
+        return ResponseUtil.getSuccessResult(data);
     }
 
     @RequestMapping("/detail/{id}")
